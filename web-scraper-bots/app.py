4eef6a46808746442cf0c5d64a91aa086fd4f3a4
Vigneshwaran Rethenasekaran <blindspot@Vigneshwarans-MacBook-Air.local>
2021-12-19 14:00:40 +0530

    Added read,edit and delete  methods for config parser

--- web-scraper-bots/app.py
+++ web-scraper-bots/app.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 class WebScraperBot:
     
     def __init__(self,host_url):
@@ -23,6 +22,10 @@ class WebScraperBot:
         except Exception:
             print("failed to Access the Host")
         return status_code,output_content
+    
+    def findTagById(self,soup_instance,idName):
+        title = soup_instance.find(id=idName)
+        return title
         
     def getAllLinksfromPage(self):
         return ""
@@ -34,4 +37,7 @@ soup_instance = BeautifulSoup(output_content, 'html.parser')
 # Pretty Print all the contends of the Page content Parsed
 #print(soup_instance.prettify())
 # Find the list of all paragraph Tags in the Page
-print(soup_instance.find_all('p'))
+#print(soup_instance.find_all('p'))
+# Find Any element in the page using the id Name - Selector
+titleElement = scraper_instance.findTagById(soup_instance,"firstHeading")
+print(titleElement.getText())
