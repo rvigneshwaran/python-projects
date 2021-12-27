877a8b3d4716e5910e6a7cddbcf0019973fa9974
Vigneshwaran Rethenasekaran <redhatvicky@gmail.com>
2021-12-27 17:49:01 +0530

    Implemented :: Find Smallest and Largest  element in the list without sorting

--- plug-utility-snippets/find-largest-element.py
+++ plug-utility-snippets/find-largest-element.py
@@ -6,12 +6,30 @@ class FindLargestElement:
     def __init__(self):
         print("Initializing Components")
         
-    def find_largest_element(self,inputList):
-        if inputList is not None:
+    def find_largest_element(self,inputList,doesWithoutSort=False):
+        if inputList is not None and not doesWithoutSort:
             inputList.sort()
             return inputList[-1]
+        elif inputList is not None and doesWithoutSort:
+            smallest_element = max(inputList)
+            return smallest_element
+        
+    def find_smallest_element(self,inputList,doesWithoutSort=False):
+        if inputList is not None and not doesWithoutSort:
+            inputList.sort()
+            return inputList[0]
+        elif inputList is not None and doesWithoutSort:
+            smallest_element = min(inputList)
+            return smallest_element
 
         
 largest_ins = FindLargestElement()
 larg_num = largest_ins.find_largest_element(input_list)
 print("The largest number in the list is :: ",larg_num)
+small_num = largest_ins.find_smallest_element(input_list)
+print("The smallest number in the list is :: ",small_num)
+
+larg_num = largest_ins.find_largest_element(input_list,True)
+print("W/O : Sort :: The largest number in the list is :: ",larg_num)
+small_num = largest_ins.find_smallest_element(input_list,True)
+print("W/O : Sort :: The smallest number in the list is :: ",small_num)
