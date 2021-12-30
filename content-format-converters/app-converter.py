82829edacd1f15143db8615afb70441109289a96
Vigneshwaran Rethenasekaran <redhatvicky@gmail.com>
2021-12-31 00:49:27 +0530

    Logic to flatten json and then convert to CSV

--- content-format-converters/app-converter.py
+++ content-format-converters/app-converter.py
@@ -1,37 +1,28 @@
 import pandas as pd
 import traceback
 import json
+from flatten_json import flatten
 
 class ContentFormatConverter:
     
     def __init__(self):
         print("Initializing Components")
         
-    def flatten_json(input_json_conends, exclude=['']):
-        output_response = {}
-        def flatten(current_element, name = '', exclude=exclude):
-            if type(current_element) is dict:
-                for indv_value in current_element:
-                    if indv_value not in exclude: flatten(current_element[indv_value], name + indv_value + '_')
-            elif type(current_element) is list:
-                current_index = 0
-                for indv_value in current_element:
-                    flatten(indv_value, name + str(current_index) + '_')
-                    current_index += 1
-            else:
-                output_response[name[:-1]] = current_element
-        flatten(input_json_conends)
-        return output_response
-            
-        
     def read_json_file(self,input_json_path,can_flatten_json):
         outpt_df = None
+        input_json_colors = None
         try:
             file_instance = open(input_json_path)
             json_data = json.load(file_instance)
             if can_flatten_json:
-                self.flatten_json(json_data)
-            input_json_colors = json_data["colors"]
+                input_json_colors = flatten(json_data)
+                input_data = json.dumps(input_json_colors)
+                input_json_colors =  json.loads(input_data)
+                output_list = []
+                output_list.append(input_json_colors)
+                input_json_colors = output_list
+            else:
+                input_json_colors = json_data["colors"]
             outpt_df = pd.DataFrame.from_records(input_json_colors)
         except:
             exception_trace = str(traceback.format_exc())
@@ -62,7 +53,7 @@ class ContentFormatConverter:
         try:
             if source_format == "CSV":
                 file_instance = pd.DataFrame(pd.read_csv(source_file_path, sep = ",", header = 0, index_col = False))
-                file_instance.to_json(output_file_path, orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
+                file_instance.to_json(output_file_path, orient = "records",indent=4)
                 print("Data Converted to JSON Successfully")
         except:
             exception_trace = str(traceback.format_exc())
@@ -79,7 +70,7 @@ source_file_path = "inputs/input-data.json"
 input_content_data["source_file_path"] = source_file_path
 output_file_path = "outputs/output-data.csv"
 input_content_data["output_file_path"] = output_file_path
-input_content_data["can_flatten_json"] = True
+input_content_data["can_flatten_json"] = False
 df_csv = converter_inst.convert_contends_csv(input_content_data)
 
 # Convert the created csv file to json again.
