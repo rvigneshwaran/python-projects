6d51340dacf95d30f0777f92623e65e97a6a1de8
Vigneshwaran Rethenasekaran <redhatvicky@gmail.com>
2022-01-06 23:50:36 +0530

    Developed Methods to parse all the links in the page URL

--- ttl-load-website/app.py
+++ ttl-load-website/app.py
@@ -3,12 +3,39 @@ import time
 import traceback
 import certifi
 import ssl
-
+from bs4 import BeautifulSoup
+import requests
 class TTLLoadWebsiteTimeAnalyzer:
     
     def __init__(self):
         print("Loading Components for the TTL Load Analyzer")
         
+    def getPageSourceAsResponse(self,websiteURL):
+        output_response = {}
+        try:
+            response_detail = requests.get(websiteURL,verify=False)
+            status_code = response_detail.status_code
+            if response_detail is not None and status_code == 200:
+                output_response["status"] = "Success"
+                output_response["output_content"] = response_detail.content
+            else:
+                output_response["status"] = "Failure"
+                output_response["error_message"] = "Failed to Access host"
+        except Exception:
+            output_response["status"] = "Failure"
+            output_response["error_message"] = str(traceback.print_exc())
+        return output_response
+        
+    def get_alllinks(self,website_url):
+        response_contends = self.getPageSourceAsResponse(website_url)
+        if response_contends is not None and response_contends["status"] == "Success":
+            output_content = response_contends["output_content"]
+            soup_instance = BeautifulSoup(output_content, 'html.parser')
+            link_comp_list = soup_instance.find_all('a')
+            for indv_element in link_comp_list[0:10]:
+                print(indv_element['class'])
+            #print(link_comp_list)
+            
     def find_time_elapsed(self,startTime,website_url):
         currentTime = time.time()
         elapsed_time = currentTime-startTime
@@ -30,3 +57,4 @@ class TTLLoadWebsiteTimeAnalyzer:
 timeelap_ins = TTLLoadWebsiteTimeAnalyzer()
 website_url = "https://en.wikipedia.org/wiki/Coimbatore"
 timeelap_ins.check_connectivity(website_url)
+timeelap_ins.get_alllinks(website_url)
