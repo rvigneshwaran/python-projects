7a0dac70b125f35ca47858ec295d49ccd0a755ce
Vigneshwaran Rethenasekaran <redhatvicky@gmail.com>
2022-01-27 20:14:23 +0530

    Updated Method Level comments

--- qr-code-generator/app.py
+++ qr-code-generator/app.py
@@ -9,10 +9,22 @@ class QRCodeGenerator:
         print("Initializing Components")
         
     def get_current_timestamp(self):
+        """[Method Intended to geerate a Timestamp as Stirng ]
+
+        Returns:
+            [str]: [Generate the Timestamp component as String]
+        """
         time_instance = time.gmtime()
         return str(calendar.timegm(time_instance))
     
     def generate_qrcode(self,input_content,file_type,input_file_name):
+        """[Method Intended to generate the QR Code Using the python external Module]
+
+        Args:
+            input_content ([type]): [input content for which the qr code is generated ]
+            file_type ([type]): [Type of the output file :: default to svg]
+            input_file_name ([type]): [the output file name ]
+        """
         generated_code = pyqrcode.create(input_content)
         time_stamp = self.get_current_timestamp()
         output_file_name = "outputs/"+input_file_name+"_"+time_stamp
