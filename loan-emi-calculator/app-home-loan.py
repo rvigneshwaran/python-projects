7d348bd3d1a536887fcdf63c1e554becafb4b231
Vigneshwaran Rethenasekaran <redhatvicky@gmail.com>
2022-01-29 12:01:53 +0530

    Updated Comments for the methods

--- loan-emi-calculator/app-home-loan.py
+++ loan-emi-calculator/app-home-loan.py
@@ -2,17 +2,47 @@ import traceback
 from num2words import num2words
 
 class HomeLoanEMIAnalyzer:
+    
     def __init__(self):
         print("Initializing Components")
         
     def getInterestAmount(self,principal,rate,timeInYears):
+        """[This Method Intended to calculate the Compound Interest that is paid for the pricipal amount paid considering the total acmount of years,Rate of Interest for the Year]
+
+        Args:
+            principal ([float]): [Initial Amount that is borowwed or Invested]
+            rate ([float]): [Rate of Interest for One Year]
+            timeInYears ([float]): [Total Time Duration Invested/ Borrowed]
+
+        Returns:
+            [float]: [Compound Interest for the amount Invested or Borrowed]
+        """
         interest_amount = principal * (pow((1+(rate/100)),timeInYears))
         return interest_amount
     
     def getTotalAmountToBePaid(self,timeInYears,monhly_amount):
+        """[This Method is Intended to calculate the total amount considering the monthly EMI amount and total time duration]
+
+        Args:
+            timeInYears ([float]): [time duration]
+            monhly_amount ([float]): [emi amount monthly]
+
+        Returns:
+            [float]: [Total Amount Paid during the Duration ]
+        """
         return (timeInYears * 12) * monhly_amount
     
     def getMonthlyEMIAmount(self,principal,rateOfInterest,timeInYears):
+        """[This Method is Intended to determine the Monthly Installment that is paid as an interest]
+
+        Args:
+            principal ([float]): [The Principal amount Invested or borroed]
+            rateOfInterest ([float]): [Rate of Interest for each Year]
+            timeInYears ([float]): [Total Time Duration]
+
+        Returns:
+            [float]: [Monthly EMI Amount]
+        """
         rateOfInterest = rateOfInterest / (12 * 100)
         timeInMonths = timeInYears * 12
         part_component = pow(1 + rateOfInterest, timeInMonths)
@@ -20,6 +50,14 @@ class HomeLoanEMIAnalyzer:
         return monthlyPayment
     
     def getNumInWords(self,inputNumber):
+        """[This Method is intended to get the Number in Words]
+
+        Args:
+            inputNumber ([Float]): [Input Nummber]
+
+        Returns:
+            [str]: [Number in Words]
+        """
         return "(" + str(num2words(int(inputNumber))) + " Rupees)"
     
 loan_emi_analyzer = HomeLoanEMIAnalyzer()
